5ec7f24d286220c544a85afe60ce43aeb3049135
Lyn-Dah <edulind99@gmail.com>
2024-07-09 11:12:39 +0000

    Rework on pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SignUp from './pages/signUp'
 import SignIn from './pages/signIn'
 import HomePage from './pages/homePage'
+import CreateEvents from './pages/createEvents'
+import EventPage from './pages/eventPage'
+import CollegeEvents from './pages/collegeEvents'
+import NotFoundPage from './pages/notFoundPage'
 
 const router = createBrowserRouter([
-    {path:"/",element:<SignUp/>},
-    {path:"signin",element:<SignIn/>},
-    {path:"homepage",element:<HomePage/>},
-    
+  { path: "/", element: <SignUp /> },
+  { path: "signin", element: <SignIn /> },
+  { path: "homepage", element: <HomePage /> },
+  { path: "createvents", element: <CreateEvents /> },
+  { path: "eventpage", element: <EventPage /> },
+  { path: "collegeevents", element: <CollegeEvents /> },
+  {path:"notfoundpage",element:<NotFoundPage/>},
+
 ])
 
 const App = () => {
   return (
     <div>
-        <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   )
 }
